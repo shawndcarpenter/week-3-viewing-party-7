3c8bdca00e811b0b11a27510e995b884bd5da7e9
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-12-11 13:28:56 -0700

    Add password digest validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User <ApplicationRecord
   validates_presence_of :email, :name 
   validates_uniqueness_of :email
   validates_presence_of :password
+  validates_presence_of :password_digest, presence: true
   has_many :viewing_parties
 
   has_secure_password
