f270ceebfcd587710db2839875da784c095b3b55
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-12-12 14:54:39 -0700

    Write tests for logging out user

---
+++ spec/features/logout_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe "User Logout" do
+  describe "Part 1: Sessions Challenge" do
+    before :each do
+      @user1 = User.create(name: "User One", email: "user1@test.com", password: "1234", password_confirmation: "1234")
+      visit login_path
+  
+      fill_in :email, with: @user1.email
+      fill_in :password, with: @user1.password
+      
+      click_button "Log In"
+  
+      expect(current_path).to eq(user_path(@user1))
+  
+      visit root_path
+    end
+
+    it 'has a link to log out' do
+      expect(current_path).to eq(root_path)
+      expect(page).to have_link("Log Out")
+      expect(page).to_not have_link("Log In")
+      expect(page).to_not have_button("Create New User")
+    end
+
+    it 'can log out a user' do
+      click_link("Log Out")
+
+      expect(current_path).to eq(root_path)
+      expect(page).to_not have_link("Log Out")
+      expect(page).to have_link("Log In")
+      expect(page).to have_button("Create New User")
+    end
+  end
+end
