d9640caa657b5ec1388e6d5c9f9a606376c7b81f
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-12-12 15:15:53 -0700

    Add tests for existing users, registered user/visitor dependent

--- spec/features/landing_page_spec.rb
+++ spec/features/landing_page_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Landing Page' do
   before :each do 
-    user1 = User.create(name: "User One", email: "user1@test.com", password: "1234", password_confirmation: "1234")
-    user2 = User.create(name: "User Two", email: "user2@test.com", password: "1234", password_confirmation: "1234")
+    @user1 = User.create(name: "User One", email: "user1@test.com", password: "1234", password_confirmation: "1234")
+    @user2 = User.create(name: "User Two", email: "user2@test.com", password: "1234", password_confirmation: "1234")
     visit '/'
   end 
 
@@ -22,15 +22,33 @@ RSpec.describe 'Landing Page' do
     expect(current_path).to eq(root_path)
   end 
 
-  it 'lists out existing users' do 
-    user1 = User.create(name: "User One", email: "user1@test.com", password: "1234", password_confirmation: "1234")
-    user2 = User.create(name: "User Two", email: "user2@test.com", password: "1234", password_confirmation: "1234")
-
+  xit 'lists out existing users' do 
     expect(page).to have_content('Existing Users:')
 
     within('.existing-users') do 
-      expect(page).to have_content(user1.email)
-      expect(page).to have_content(user2.email)
+      expect(page).to have_content(@user1.email)
+      expect(page).to have_content(@user2.email)
     end     
   end 
+
+  describe "Authorization Challenge" do
+    it "does not show existing users as a visitor" do
+      expect(page).to_not have_content(@user1.email)
+      expect(page).to_not have_content(@user2.email)
+      expect(page).to_not have_content("Existing Users:")
+    end
+
+    it "does not show links to show pages for registered users" do
+      visit login_path
+      fill_in :email, with: @user1.email
+      fill_in :password, with: @user1.password    
+      click_button "Log In" 
+      visit root_path
+
+      expect(page).to have_content(@user1.email)
+      expect(page).to have_content(@user2.email)
+      expect(page).to_not have_link(@user1.email)
+      expect(page).to_not have_link(@user2.email)
+    end
+  end
 end
