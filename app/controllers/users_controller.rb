4534b4b7d09815b677cda65ee73175d3b2152a63
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-12-12 15:48:38 -0700

    Add MVC functionality for attempted viewing of unauthorized pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,10 @@ class UsersController <ApplicationController
 
   def show 
     @user = User.find(params[:id])
+    unless @user == current_user?
+      redirect_to root_path
+      flash[:error] = "You must be logged in or registered to access the dashboard."
+    end
   end 
 
   def login_form
--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -2,10 +2,16 @@ class ViewingPartiesController < ApplicationController
   def new
     @user = User.find(params[:user_id])
     @movie = Movie.find(params[:movie_id])
+
+    unless @user == current_user?
+      redirect_to "/users/#{params[:user_id]}/movies/#{params[:movie_id]}"
+      flash[:error] = "You must be logged in or registered to create a movie party."
+    end
   end 
   
   def create 
-    user = User.find(params[:user_id])
+    @user = User.find(params[:user_id])
+
     user.viewing_parties.create(viewing_party_params)
     redirect_to "/users/#{params[:user_id]}"
   end 
